6210ff75c3e15dbe90ef0b071125dc57e228d895
puzplaiaundi <puzquiano@plaiaundi.com>
2026-09-15 19:30:41 +0200

    initial commit

---
+++ Tema1/js/app.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("btn");
+console.log("app")
+if (button) {
+    button.addEventListener("click", function () {
+        alert("Aplicación iniciada");
+    });
+}
